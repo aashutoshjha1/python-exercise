88ca219761bac8580d8ce6fc4691dedf94d058a7
aashutosh.jha <aashutosh.jha@walmart.com>
2022-10-05 20:00:13 +0530

    added exercise

---
+++ Chapter1/C-1.13_reverse_list.py
@@ -0,0 +1,11 @@
+#!/usr/env python3
+def reverse_list(n:list):
+     """ Create reverse of list without using inbuilt function"""
+     reverse_list = []
+     for i in n:
+         reverse_list.insert(0,i)
+     return reverse_list
+
+list1 = [2,3,4,5,6]
+print(reverse_list(list1))
+
